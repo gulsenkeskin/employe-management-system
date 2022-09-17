7aeb87dea95759c0ad011030d67872a88f381744
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-17 20:00:33 +0300

    clear all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DialogComponent } from './dialog/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +8,7 @@ import { DialogComponent } from './dialog/dialog/dialog.component';
 export class AppComponent {
   title = 'employe-management-system';
 
-  constructor(private dialog: MatDialog) { }
+  constructor() { }
 
-
-
-  openDialog() {
-    this.dialog.open(DialogComponent, {
-      width: "30%"
-    }).afterClosed().subscribe(val => {
-      //close da gönderdğimiz değer buraya düşer
-
-    })
-  }
 
 }
