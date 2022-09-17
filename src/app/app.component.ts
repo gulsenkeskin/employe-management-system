e45705945cfcb111116ede57387069dab80f0882
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-17 20:44:42 +0300

    added dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { MatDialog, } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'employe-management-system';
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-
+  openDialog() {
+    this.dialog.open(DialogComponent);
+  }
 }
---
+++ src/app/dialog/dialog/dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  constructor() { }
+  // constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  ngOnInit(): void {
+  }
+
+}
